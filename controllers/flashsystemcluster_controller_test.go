60a87fa7d3b340d28cf6c5feb022ac1e5dfe803b
Bo Liu <lbseraph@gmail.com>
2021-07-02 17:29:01 +0800

    Update go dependencies

--- controllers/flashsystemcluster_controller_test.go
+++ controllers/flashsystemcluster_controller_test.go
@@ -23,11 +23,11 @@ import (
 	//	"fmt"
 	"time"
 
+	odfv1alpha1 "github.com/IBM/ibm-storage-odf-operator/api/v1alpha1"
+	"github.com/IBM/ibm-storage-odf-operator/controllers/util"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	odfv1alpha1 "github.com/IBM/ibm-storage-odf-operator/api/v1alpha1"
-	"github.com/IBM/ibm-storage-odf-operator/controllers/util"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
