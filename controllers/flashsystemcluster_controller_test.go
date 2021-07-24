ea590dada474706c5593896fea15c8c5c67676d3
Ning Ding <shdn@cn.ibm.com>
2021-07-24 17:51:56 +0800

    fix: rename the management address key
    Signed-off-by: shdn@cn.ibm.com

--- controllers/flashsystemcluster_controller_test.go
+++ controllers/flashsystemcluster_controller_test.go
@@ -85,7 +85,7 @@ var _ = Describe("FlashSystemClusterReconciler", func() {
 					Namespace: namespace,
 				},
 				Data: map[string][]byte{
-					"management_address": []byte("OS4xMTAuNzAuOTY="),
+					"managementAddress": []byte("OS4xMTAuNzAuOTY="),
 					"password":           []byte("ZnNkcml2ZXI="),
 					"username":           []byte("ZnNkcml2ZXI="),
 				},
