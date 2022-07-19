ed777d7034e60d05b1abcf01566f0d5b7b02cd4f
Alon Firestein <alon.firestein@ibm.com>
2022-07-19 15:29:46 +0300

    added use of variable and copy cr file into samples folder

--- hack/ensure-blockcsi-cryaml.sh
+++ hack/ensure-blockcsi-cryaml.sh
@@ -21,11 +21,13 @@ set -e
 source hack/common.sh
 
 CSI_CR_PATH="config/manager/${BLOCK_CSI_CR_FILE}"
-CSI_CR_URL="https://raw.githubusercontent.com/IBM/ibm-block-csi-operator/${BLOCK_CSI_RELEASE}/config/samples/csi.ibm.com_v1_ibmblockcsi_cr.yaml"
+CSI_SAMPLES_CR_PATH="config/samples/${BLOCK_CSI_CR_FILE}"
+CSI_CR_URL="https://raw.githubusercontent.com/IBM/ibm-block-csi-operator/${BLOCK_CSI_RELEASE}/config/samples/${BLOCK_CSI_CR_FILE}"
 
 if curl --head --silent --fail ${CSI_CR_URL} 2> /dev/null; then
         echo "Downloading the IBM Block CSI CR file..."
         curl -JL "${CSI_CR_URL}" -o "${CSI_CR_PATH}"
+        curl -JL "${CSI_CR_URL}" -o "${CSI_SAMPLES_CR_PATH}"
 else
         echo "Using the cached CR file ${BLOCK_CSI_CR_FILE}"
 fi
