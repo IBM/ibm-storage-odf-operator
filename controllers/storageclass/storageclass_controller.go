a7264170cdaf38d210eeb30571f150e79a1642a6
Alon Firestein <alon.firestein@ibm.com>
2022-10-11 09:58:33 +0300

    fixed error logs for getSecret and checks that name and namespace are of same type
    Signed-off-by: Alon Firestein <alon.firestein@ibm.com>

--- controllers/storageclass/storageclass_controller.go
+++ controllers/storageclass/storageclass_controller.go
@@ -189,20 +189,27 @@ func (r *StorageClassWatcher) getFlashSystemClusterByStorageClass(sc *storagev1.
 
 func (r *StorageClassWatcher) getSecret(sc *storagev1.StorageClass) (corev1.Secret, error) {
 	secret := &corev1.Secret{}
+	isSamePrefixCheck := false
 	secretName, ok := sc.Parameters[util.DefaultSecretNameKey]
 	if !ok {
 		secretName, ok = sc.Parameters[util.ProvisionerSecretNameKey]
 		if !ok {
-			return *secret, fmt.Errorf("cannot find/identify secret name in StorageClass")
+			return *secret, fmt.Errorf("cannot find secret name in StorageClass")
 		}
+		isSamePrefixCheck = true
 	}
 	secretNamespace, ok := sc.Parameters[util.DefaultSecretNamespaceKey]
-	if !ok {
+	if !ok && isSamePrefixCheck {
 		secretNamespace, ok = sc.Parameters[util.ProvisionerSecretNamespaceKey]
 		if !ok {
-			return *secret, fmt.Errorf("cannot find/identify secret namespace in StorageClass")
+			return *secret, fmt.Errorf("cannot find secret namespace in StorageClass")
 		}
+		isSamePrefixCheck = false
 	}
+	if isSamePrefixCheck {
+		return *secret, fmt.Errorf("cannot find secret namespace in StorageClass")
+	}
+
 	err := r.Client.Get(context.Background(),
 		types.NamespacedName{
 			Namespace: secretNamespace,
