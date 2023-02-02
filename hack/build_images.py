8bce08cec751393cb73c2d37944f70813a65ec17
Tal Yichye <tal.yichye@ibm.com>
2023-02-02 19:20:00 +0200

    extract strings to const and refactor build images script
    Signed-off-by: Tal Yichye <tal.yichye@ibm.com>

--- hack/build_images.py
+++ hack/build_images.py
@@ -8,68 +8,72 @@ import yaml
 from yaml.loader import SafeLoader
 
 
-def build_and_push_operator_image(docker_registry, git_branch,  driver_image, console_image, platform, output_file):
-    git_url = 'https://github.com/IBM/ibm-storage-odf-operator.git'
-    docker_repo_name = 'ibm-storage-odf-operator'
-    local_dir = 'ibm-storage-odf-operator'
+OPERATOR_GIT_URL = 'https://github.com/IBM/ibm-storage-odf-operator.git'
+DRIVER_GIT_URL = 'https://github.com/IBM/ibm-storage-odf-block-driver.git'
+CONSOLE_GIT_URL = 'https://github.com/IBM/ibm-storage-odf-console.git'
 
-    print("pre-cleanup")
-    if os.path.exists(local_dir):
-        rmtree(local_dir)
+DRIVER_DOCKER_REPO_NAME = 'ibm-storage-odf-block-driver'
+CONSOLE_DOCKER_REPO_NAME = 'ibm-storage-odf-plugin'
+OPERATOR_DOCKER_REPO_NAME = 'ibm-storage-odf-operator'
+CATALOG_DOCKER_REPO_NAME = 'ibm-storage-odf-catalog'
 
-    print("Cloning git project")
-    git.Repo.clone_from(git_url, local_dir, branch=git_branch)
+DRIVER_CLONE_LOCAL_DIR = 'ibm-storage-odf-block-driver'
+CONSOLE_CLONE_LOCAL_DIR = 'ibm-storage-odf-console'
+OPERATOR_CLONE_LOCAL_DIR = 'ibm-storage-odf-operator'
 
-    print("Generating image tag")
+OUTPUT_FILE_NAME = 'odf-images-url.txt'
+
+OPERATOR_CSV_PATH = f'{OPERATOR_CLONE_LOCAL_DIR}/bundle/manifests/ibm-storage-odf-operator.clusterserviceversion.yaml'
+
+
+def build_and_push_operator_image(docker_registry, git_branch,  driver_image, console_image, platform):
+    print("Cloning git project", flush=True)
+    git.Repo.clone_from(OPERATOR_GIT_URL, OPERATOR_CLONE_LOCAL_DIR, branch=git_branch)
+
+    print("Generating image tag", flush=True)
     image_tag = generate_image_tag(git_branch)
 
     print("Building image")
-    env_vars = dict(IMAGE_REGISTRY=docker_registry, IMAGE_TAG=image_tag, **os.environ)
-    subprocess.run(["make", "-C", f'{local_dir}/', "build"], env=env_vars, check=True)
-    subprocess.run(["make", "-C", f'{local_dir}/', "docker-build"], env=env_vars, check=True)
-    subprocess.run(["make", "-C", f'{local_dir}/', "bundle-build"], env=env_vars, check=True)
-    edit_operator_csv(local_dir, driver_image, console_image)
-    subprocess.run(["make", "-C", f'{local_dir}/', "catalog-build"], env=env_vars, check=True)
+    env_vars = dict(IMAGE_REGISTRY=docker_registry, IMAGE_TAG=image_tag, BUILD_PLATFORM=platform, **os.environ)
+    run_operator_make_command("build", env_vars)
+    run_operator_make_command("docker-build", env_vars)
+    run_operator_make_command("bundle-build", env_vars)
+    edit_operator_csv(driver_image, console_image)
+    run_operator_make_command("catalog-build", env_vars)
 
-    print("Writing image name to images file")
-    image_name = docker_registry + '/' + docker_repo_name + ':' + image_tag
-    with open(output_file, "a") as images_file:
-        images_file.write(image_name + "\n")
-
-    print("post-cleanup")
-    if os.path.exists(local_dir):
-        rmtree(local_dir)
-
-    return image_name
+    print("Dumping created image name", flush=True)
+    operator_image_name = f'{docker_registry}/{OPERATOR_DOCKER_REPO_NAME}:{image_tag}'
+    catalog_image_name = f'{docker_registry}/{CATALOG_DOCKER_REPO_NAME}:{image_tag}'
+    with open(OUTPUT_FILE_NAME, "a") as images_file:
+        images_file.write(operator_image_name + "\n")
+        images_file.write(catalog_image_name + "\n")
 
 
-def edit_operator_csv(operator_local_dir, driver_image, console_image):
-    csv_path = f'{operator_local_dir}/bundle/manifests/ibm-storage-odf-operator.clusterserviceversion.yaml'
+def run_operator_make_command(make_command, env_vars):
+    subprocess.run(["make", "-C", f'{OPERATOR_CLONE_LOCAL_DIR}/', make_command], env=env_vars, check=True)
 
-    with open(csv_path) as f:
+
+def edit_operator_csv(driver_image, console_image):
+    with open(OPERATOR_CSV_PATH) as f:
         csv = yaml.load(f, Loader=SafeLoader)
         csv["spec"]["install"]["spec"]["deployments"][1]["spec"]["template"]["spec"]["containers"][0]["image"] = console_image
         csv["spec"]["install"]["spec"]["deployments"][0]["spec"]["template"]["spec"]["containers"][1]["env"][0]["value"] = driver_image
 
-    with open(csv_path, mode="w") as f:
+    with open(OPERATOR_CSV_PATH, mode="w") as f:
         yaml.dump(csv, f)
 
 
-def build_and_push_driver_image(docker_registry, git_branch, platform, output_file):
-    driver_git_url = 'https://github.com/IBM/ibm-storage-odf-block-driver.git'
-    driver_docker_repo = 'ibm-storage-odf-block-driver'
-    driver_local_dir = 'ibm-storage-odf-block-driver'
-    build_and_push_image(docker_registry, driver_docker_repo, driver_git_url, git_branch, driver_local_dir, platform, output_file)
+def build_and_push_driver_image(docker_registry, git_branch, platform):
+    build_and_push_image(docker_registry, DRIVER_DOCKER_REPO_NAME, DRIVER_GIT_URL, git_branch,
+     DRIVER_CLONE_LOCAL_DIR, platform)
 
 
-def build_and_push_console_image(docker_registry, git_branch, platform, output_file):
-    console_git_url = 'https://github.com/IBM/ibm-storage-odf-console.git'
-    console_docker_repo = 'ibm-storage-odf-plugin'
-    console_local_dir = 'ibm-storage-odf-console'
-    build_and_push_image(docker_registry, console_docker_repo, console_git_url, git_branch, console_local_dir, platform, output_file)
+def build_and_push_console_image(docker_registry, git_branch, platform):
+    build_and_push_image(docker_registry, CONSOLE_DOCKER_REPO_NAME, CONSOLE_GIT_URL, git_branch,
+     CONSOLE_CLONE_LOCAL_DIR, platform)
 
 
-def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch, local_dir, platform, output_file):
+def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch, local_dir, platform):
     """
     build and push odf console/driver docker image
     :param docker_registry: docker registry name including namespace (if exist) to push the image into. e.g. docker.io/tyichye
@@ -78,13 +82,7 @@ def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch,
     :param git_branch: branch name to build the image from. e.g. release-1.3.0
     :param local_dir: local directory to clone the git repo into
     :param platform: docker build platform. linux/amd64, linux/ppc64le, linux/s390x
-    :param output_file: Jenkins archive file to dumping the generated image name
-    :return: created image name
     """
-
-    print("pre-cleanup", flush=True)
-    if os.path.exists(local_dir):
-        rmtree(local_dir)
 
     print("Cloning git project", flush=True)
     git.Repo.clone_from(git_url, local_dir, branch=git_branch)
@@ -99,16 +97,10 @@ def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch,
                     f"TARGET_BRANCH={git_branch}",
                     f"PLATFORM={platform}"], check=True)
 
-    print("Writing image name to images file", flush=True)
-    image_name = docker_registry + '/' + docker_repo_name + ':' + image_tag
-    with open(output_file, "a") as images_file:
+    print("Dumping created image name", flush=True)
+    image_name = f'{docker_registry}/{docker_repo_name}:{image_tag}'
+    with open(OUTPUT_FILE_NAME, "a") as images_file:
         images_file.write(image_name + "\n")
-
-    print("post-cleanup", flush=True)
-    if os.path.exists(local_dir):
-        rmtree(local_dir)
-
-    return image_name
 
 
 def generate_image_tag(branch_name):
@@ -117,3 +109,15 @@ def generate_image_tag(branch_name):
     generated_branch_name = branch_name.split("/")[-1]
     image_tag = (generated_dt + "-" + generated_branch_name)[:128]
     return image_tag
+
+
+def cleanup_env():
+    print("cleanup env", flush=True)
+    if os.path.exists(DRIVER_CLONE_LOCAL_DIR):
+        rmtree(DRIVER_CLONE_LOCAL_DIR)
+    if os.path.exists(CONSOLE_CLONE_LOCAL_DIR):
+        rmtree(CONSOLE_CLONE_LOCAL_DIR)
+    if os.path.exists(OPERATOR_CLONE_LOCAL_DIR):
+        rmtree(OPERATOR_CLONE_LOCAL_DIR)
+    if os.path.exists(OUTPUT_FILE_NAME):
+        os.remove(OUTPUT_FILE_NAME)
