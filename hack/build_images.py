f2759742e3817101567b0db6b8e077c9874321cf
Tal Yichye <tal.yichye@ibm.com>
2023-02-06 14:34:41 +0200

    refactor build-image script
    Signed-off-by: Tal Yichye <tal.yichye@ibm.com>

--- hack/build_images.py
+++ hack/build_images.py
@@ -21,14 +21,11 @@ DRIVER_CLONE_LOCAL_DIR = 'ibm-storage-odf-block-driver'
 CONSOLE_CLONE_LOCAL_DIR = 'ibm-storage-odf-console'
 OPERATOR_CLONE_LOCAL_DIR = 'ibm-storage-odf-operator'
 
-OPERATOR_CSV_PATH = f'{OPERATOR_CLONE_LOCAL_DIR}/bundle/manifests/ibm-storage-odf-operator.clusterserviceversion.yaml'
-
 
 def build_and_push_operator_image(docker_registry, git_branch,  driver_image, console_image, platform, output_file):
     print("Cloning git project", flush=True)
     git.Repo.clone_from(OPERATOR_GIT_URL, OPERATOR_CLONE_LOCAL_DIR, branch=git_branch)
 
-    print("Generating image tag", flush=True)
     image_tag = generate_image_tag(git_branch)
     driver_image_tag = driver_image.split(":")[-1]
     console_image_tag = console_image.split(":")[-1]
@@ -42,26 +39,13 @@ def build_and_push_operator_image(docker_registry, git_branch,  driver_image, co
     run_operator_make_command("bundle-build", env_vars)
     run_operator_make_command("build-catalog", env_vars)
 
-    print("Dumping created image name", flush=True)
     operator_image_name = f'{docker_registry}/{OPERATOR_DOCKER_REPO_NAME}:{image_tag}'
     catalog_image_name = f'{docker_registry}/{CATALOG_DOCKER_REPO_NAME}:{image_tag}'
-    with open(output_file, "a") as images_file:
-        images_file.write(operator_image_name + "\n")
-        images_file.write(catalog_image_name + "\n")
-
+    writeCreatedImageToOutputFile(output_file, operator_image_name)
+    writeCreatedImageToOutputFile(output_file, catalog_image_name)
 
 def run_operator_make_command(make_command, env_vars):
     subprocess.run(["make", "-C", f'{OPERATOR_CLONE_LOCAL_DIR}/', make_command], env=env_vars, check=True)
-
-
-def edit_operator_csv(driver_image, console_image):
-    with open(OPERATOR_CSV_PATH) as f:
-        csv = yaml.load(f, Loader=SafeLoader)
-        csv["spec"]["install"]["spec"]["deployments"][1]["spec"]["template"]["spec"]["containers"][0]["image"] = console_image
-        csv["spec"]["install"]["spec"]["deployments"][0]["spec"]["template"]["spec"]["containers"][1]["env"][0]["value"] = driver_image
-
-    with open(OPERATOR_CSV_PATH, mode="w") as f:
-        yaml.dump(csv, f)
 
 
 def build_and_push_driver_image(docker_registry, git_branch, platform, output_file):
@@ -89,7 +73,6 @@ def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch,
     print("Cloning git project", flush=True)
     git.Repo.clone_from(git_url, local_dir, branch=git_branch)
 
-    print("Generating image tag", flush=True)
     image_tag = generate_image_tag(git_branch)
 
     print("Building image", flush=True)
@@ -99,27 +82,19 @@ def build_and_push_image(docker_registry, docker_repo_name, git_url, git_branch,
                     f"TARGET_BRANCH={git_branch}",
                     f"PLATFORM={platform}"], check=True)
 
-    print("Dumping created image name", flush=True)
     image_name = f'{docker_registry}/{docker_repo_name}:{image_tag}'
-    with open(output_file, "a") as images_file:
-        images_file.write(image_name + "\n")
+    writeCreatedImageToOutputFile(output_file, image_name)
 
 
 def generate_image_tag(branch_name):
+    print("Generating image tag", flush=True)
     now = datetime.now()
     generated_dt = now.strftime("%d.%m.%y-%H.%M")
     generated_branch_name = branch_name.split("/")[-1]
     image_tag = (generated_dt + "-" + generated_branch_name)[:128]
     return image_tag
 
-
-def cleanup_env(output_file):
-    print("cleanup env", flush=True)
-    if os.path.exists(DRIVER_CLONE_LOCAL_DIR):
-        rmtree(DRIVER_CLONE_LOCAL_DIR)
-    if os.path.exists(CONSOLE_CLONE_LOCAL_DIR):
-        rmtree(CONSOLE_CLONE_LOCAL_DIR)
-    if os.path.exists(OPERATOR_CLONE_LOCAL_DIR):
-        rmtree(OPERATOR_CLONE_LOCAL_DIR)
-    if os.path.exists(output_file):
-        os.remove(output_file)
+def writeCreatedImageToOutputFile(output_file, image_name):
+    print("Dumping created image name", flush=True)
+    with open(output_file, "a") as images_file:
+        images_file.write(image_name + "\n")
