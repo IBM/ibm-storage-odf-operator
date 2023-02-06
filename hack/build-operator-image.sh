5298906f46afd3eaef61ca43571b41615ef364e9
Tal Yichye <tal.yichye@ibm.com>
2023-02-06 14:35:26 +0200

    add seperate tags for console and driver
    Signed-off-by: Tal Yichye <tal.yichye@ibm.com>

--- hack/build-operator-image.sh
+++ hack/build-operator-image.sh
@@ -20,9 +20,6 @@ set -e
 
 source hack/common.sh
 
-echo "Building Operator image ${OPERATOR_FULL_IMAGE_NAME}..."
-docker buildx build -t "${OPERATOR_FULL_IMAGE_NAME}" --build-arg VCS_REF=${VCS_REF} --build-arg VCS_URL=${VCS_URL} --platform ${PLATFORM} .
+echo "Building Operator image and Pushing to image registry ${OPERATOR_FULL_IMAGE_NAME}..."
+docker buildx build -t "${OPERATOR_FULL_IMAGE_NAME}" --build-arg VCS_REF=${VCS_REF} --build-arg VCS_URL=${VCS_URL} --platform ${PLATFORM} . --push
 
-echo
-echo "Pushing Operator image to image registry..."
-docker push "${OPERATOR_FULL_IMAGE_NAME}"
