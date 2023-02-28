3fd7721fa834050079e20136543a3174b724a8c7
662962756 <bvered@il.ibm.com>
2023-02-28 10:52:35 +0200

    rebuild ensure-opm
    Signed-off-by: 662962756 <bvered@il.ibm.com>

--- hack/build-operator-bundle.sh
+++ hack/build-operator-bundle.sh
@@ -63,6 +63,7 @@ check_and_build_csi_bundle_image(){
     override_csi_csi_file
 
     cd "${oldPWD}/${CSI_OPERATOR_IMAGE_NAME}"
+    source hack/ensure-opm.sh
     build_push_bundle_image "${IMAGE_REGISTRY}/${CSI_DEVELOP_BUNDLE_FULL_IMAGE_NAME}:${IMAGE_TAG}" "${CSI_CSV_PATH}" "${CSI_OPERATOR_IMAGE_NAME}" "${CSI_CHANNEL}"
     echo
 
