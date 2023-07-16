8efe83d9eb9cf2469a01f003967c765413f7a590
662962756 <bvered@il.ibm.com>
2023-07-16 10:38:11 +0300

    add permissions for pods
    Signed-off-by: 662962756 <bvered@il.ibm.com>

--- controllers/flashsystemcluster_controller.go
+++ controllers/flashsystemcluster_controller.go
@@ -189,6 +189,7 @@ type FlashSystemClusterReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=events,verbs=get;list;watch;create;update;patch
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
