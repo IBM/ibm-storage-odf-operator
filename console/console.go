9fecf472b5910724ac5c2fda3a30c4006e83b375
Shlomit Neufeld <shlomitn@il.ibm.com>
2025-06-23 12:53:09 +0300

    bump version: ODF 1.7.1 -> 1.8.0, CSI 1.12.2/3 -> 1.12.4, max OCP 4.18 -> 4.19
    Signed-off-by: Shlomit Neufeld <shlomitn@il.ibm.com>

--- console/console.go
+++ console/console.go
@@ -127,7 +127,7 @@ func RemoveConsole(client client.Client, namespace string) error {
 	return nil
 }
 func GetBasePath(clusterVersion string) string {
-	if strings.Contains(clusterVersion, "4.18") {
+	if strings.Contains(clusterVersion, "4.20") {
 		return CompatibilityBasePath
 	}
 
